2674df2ad87aa12955914b150bd7bc06460d1f4d
Savio Ferreira <saviio.ferreiira15@gmail.com>
2021-12-29 13:31:57 -0300

    adicionando novos arquivos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import { InferGetStaticPropsType } from "next";
 import Head from "next/head";
+import Link from 'next/link'
 import Header from "@components/header";
 import Navigation from "@components/navigation"
+import { getPostList } from "@shared/util";
 
+type PostList = string[]
 
-export default function Home() {
+export default function Home({ posts }:InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
       <Head>
@@ -13,7 +17,29 @@ export default function Home() {
       <main>
         <Navigation />
         <Header />
+        {posts.length > 0 && (
+          <ul>
+            {posts.map((slug) => (
+              <li key={slug}>
+                <Link href={`post/${slug}`}>
+                <a>
+                {slug.replace('-', ' ')}
+                </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
       </main>
     </>
   );
+}
+
+export const getStaticProps = async () => {
+  const posts:PostList = getPostList()
+  return{
+    props: {
+      posts
+    }
+  }
 }
---
+++ pages/post/hello-there.tsx
@@ -0,0 +1,7 @@
+const Hello = () => (
+    <div>
+        <h1>Hello there</h1>
+    </div>
+)
+
+export default Hello
